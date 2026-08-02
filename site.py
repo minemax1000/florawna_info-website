f2a754477d76c5de393a88880955f51c4911da6d
minemax1000 <minemax1000@gmail.com>
2026-08-02 23:03:24 +0300

    Add files via upload

---
+++ site.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+import os
+
+app = Flask(__name__)
+
+@app.route("/guide")
+def guide():
+    return render_template("guide.html")
+
+@app.route("/map")
+def map():
+    return render_template("map.html")
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
