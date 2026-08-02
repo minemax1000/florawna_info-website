3718dc07d83d42d2e8b9d8721330a7567c45e055
minemax1000 <minemax1000@gmail.com>
2026-08-02 23:11:30 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -3,6 +3,10 @@ import os
 
 app = Flask(__name__)
 
+@app.route("/")
+def home():
+    return render_template("main.html")
+
 @app.route("/guide")
 def guide():
     return render_template("guide.html")
